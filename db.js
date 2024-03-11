a88c9fd6e87974fc2541367f838e97553efc760b
noobwebdev777 <125580549+noobwebdev777@users.noreply.github.com>
2024-03-11 22:25:01 +0530

    db fixed lol

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const connect = mysql.createConnection({
     host: 'database-1.cz0iiki2sghw.eu-north-1.rds.amazonaws.com',
     user: 'admin',
-    password: 'rahul2021',
+    password: '',
     database: 'awsnode'
 })
 
